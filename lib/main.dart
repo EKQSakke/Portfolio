d8fac49c5f4ed138c2ce6c75cf052dbdc0d9483f
Niteb <ekq.sakke@gmail.com>
2020-11-25 22:48:11 +0200

    - Hero section mostly done, text just not selectble correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'style.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sakari Ekqvist - Portfolio',
+      title: 'Sakari Ekqvist',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
@@ -24,23 +26,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _scrollController = ScrollController();
+  final double _scrollHeightProjects = 1375;
+  final double _scrollHeightContact = 1375;
+
+  void scrollTo(double height) {
+    _scrollController.animateTo(height, duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var screen = MediaQuery.of(context).size;
+
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: colorBG,
       body: SingleChildScrollView(
+        controller: _scrollController,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Padding(
+              // Nav Bar
+              padding: const EdgeInsets.all(24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () => scrollTo(_scrollHeightProjects),
+                    child: Text(
+                      'Projects',
+                      style: navbarText,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(48, 0, 176, 0),
+                    child: SelectableText(
+                      'Contact',
+                      style: navbarText,
+                    ),
+                  )
+                ],
+              ),
+            ),
             Container(
-              color: Colors.white,
-                margin: EdgeInsets.all(100),
-                height: 500,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                margin: EdgeInsets.fromLTRB(200, 200, 200, 200),
+                height: screen.height * .75,
+                child: Stack(
                   children: [
-                    Text("Hello"),
-                    Text("I'm <b>Sakari Ekqvist, a GAME and APP developer from Turku, Finland"),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                          height: 48,
+                          width: 48,
+                          child: InkWell(
+                              onTap: () => scrollTo(_scrollHeightProjects), child: Image.asset('images/github.png'))),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Flexible(
+                              flex: 3,
+                              fit: FlexFit.loose,
+                              child: Expanded(
+                                flex: 3,
+                                child: Container(
+                                  child: Stack(
+                                    children: [
+                                      Transform.rotate(
+                                        angle: 1,
+                                        child: Text(
+                                          '01010011 01100001\n01101011 01100001\n01110010 01101001\n00100000 01000101\n01101011 01110001\n01110110 01101001\n01110011 01110100',
+                                          style: fadeText,
+                                        ),
+                                      ),
+                                      Column(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+                                            child: SelectableText(
+                                              "Sakari Ekqvist",
+                                              style: TextStyle(
+                                                  fontSize: 48,
+                                                  decoration: TextDecoration.underline,
+                                                  color: Colors.white,
+                                                  fontFamily: 'RobotoMono'),
+                                            ),
+                                          ),
+                                          Padding(
+                                              padding: EdgeInsets.all(12),
+                                              child: Stack(
+                                                children: [
+                                                  Container(
+                                                    height: 48,
+                                                  ),
+                                                  SocialButton(
+                                                      imageName: 'images/github.png', link: 'github account page'),
+                                                  Padding(
+                                                    padding: const EdgeInsets.fromLTRB(52, 0, 0, 0),
+                                                    child: SocialButton(
+                                                        imageName: 'images/linkedin.png', link: 'linkedin page'),
+                                                  ),
+                                                ],
+                                              )),
+                                          SelectableText(
+                                            "- Programmer\n- Unity\n- Flutter",
+                                            style: blueText,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Flexible(
+                              flex: 3,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SelectableText.rich(TextSpan(text: 'Hi! ', style: mainText, children: [
+                                    TextSpan(
+                                        text: 'I’m a Turku-based game/software developer.' +
+                                            'I have experience working in Unity, Flutter, ' +
+                                            'and various other environments.' +
+                                            '\n\nGot an interesting project?\nContact me at:\n',
+                                        style: blueText),
+                                    TextSpan(text: 'sakari.ekqvist@gmail.com')
+                                  ])),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ],
                 )),
             Container(color: Colors.blue, margin: EdgeInsets.all(25), height: 500, child: Text("Hello")),
@@ -53,6 +183,56 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class SocialButton extends StatefulWidget {
+  final imageName;
+  final link;
+
+  const SocialButton({
+    Key key,
+    @required this.imageName,
+    @required this.link,
+  }) : super(key: key);
+
+  @override
+  _SocialButtonState createState() => _SocialButtonState();
+}
+
+class _SocialButtonState extends State<SocialButton> {
+  double _size = 24;
+  double _margins = 12;
+
+  @override
+  void setState(fn) {
+    super.setState(fn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => print('Open ${widget.link}'),
+      onHover: (value) {
+        if (value){
+          _size = 48;
+          _margins = 0;
+        }
+        else{
+          _size = 24;
+          _margins = 12;
+        }
+
+        print(value);
+        setState(() {});
+      },
+      child: Container(
+          margin: EdgeInsets.fromLTRB(_margins, _margins, 0, 0),
+          color: Colors.transparent,
+          width: _size,
+          height: _size,
+          child: Image.asset(widget.imageName)),
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color colorBG = Color.fromARGB(255, 51, 51, 51);
+Color colorText = Color.fromARGB(255, 196, 196, 196);
+Color colorBlueText = Color.fromARGB(255, 173, 250, 245);
+
+TextStyle mainText = TextStyle(color: Colors.white, fontSize: 36, fontFamily: 'RobotoMono');
+TextStyle fadeText = TextStyle(color: Colors.white10, fontSize: 52, fontFamily: 'RobotoMono');
+TextStyle blueText = TextStyle(color: colorBlueText, fontSize: 36, fontFamily: 'RobotoMono');
+TextStyle navbarText = TextStyle(color: colorBlueText, fontSize: 24, fontFamily: 'RobotoMono');
